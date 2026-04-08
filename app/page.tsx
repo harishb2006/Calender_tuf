8dff0a231918b50e892f72eded0301a7fda115b1
harishb2006 <imharishba@gmail.com>
2026-04-08 12:45:23 +0530

    i have completed this two components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+import React, { useState } from 'react';
+import CalendarGrid from "./Components/calender/calenderGrid";
+import CalendarImageCard from "./Components/calender/calenderImage";
 
+const Page = () => {
+  const [viewDate, setViewDate] = useState(new Date());
 
-export default function Home() {
   return (
-    <div>
-     hello
+    <div className="min-h-screen flex items-center justify-center p-4">
+      <div className="flex flex-col md:flex-row items-start justify-center gap-8 w-full max-w-4xl">
+        <div className="w-full md:w-1/2">
+          <CalendarGrid viewDate={viewDate} setViewDate={setViewDate} />
+        </div>
+        <div className="w-full md:w-1/2 h-[500px] md:h-[600px] rounded-2xl overflow-hidden shadow-xl">
+          <CalendarImageCard viewDate={viewDate} />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
+
+export default Page
